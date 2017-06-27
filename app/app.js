23596896d7967e7e7e2ba62c75f6c71a1563b9ae
krsdata <krsdata.net@gmail.com>
2017-06-27 11:05:19 +0530

    setup

---
+++ app/app.js
@@ -0,0 +1,20 @@
+﻿var app = angular.module('growSure', ['ngRoute']);
+
+
+app.config(function ($routeProvider) {
+    $routeProvider
+    .when("/", {
+        templateUrl: "app/views/dashboard.html"
+    })
+    
+    .otherwise({
+        redirect: '/'
+    });;
+});
+
+app.config(function ($httpProvider) {
+    $httpProvider.defaults.headers.common = {};
+    $httpProvider.defaults.headers.post = {};
+    $httpProvider.defaults.headers.put = {};
+    $httpProvider.defaults.headers.patch = {};
+});
